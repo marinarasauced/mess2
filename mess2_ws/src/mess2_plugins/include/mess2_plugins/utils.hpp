074b591411022bb3c1d26d544dfe92cdd6cfb93f
Marina Nelson <mjnelson@wpi.edu>
2024-08-16 12:36:16 -0400

    add utils plugin

---
+++ mess2_ws/src/mess2_plugins/include/mess2_plugins/utils.hpp
@@ -0,0 +1,22 @@
+#ifndef MESS2_PLUGINS__UTILS_HPP_
+#define MESS2_PLUGINS__UTILS_HPP_
+
+#include <cmath>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <queue>
+#include <string>
+#include <thread>
+#include <tuple>
+#include <vector>
+
+namespace mess2_plugins {
+
+std::string get_vicon_topic(std::string agent_name);
+
+}  // namespace mess2_plugins
+
+#endif  // MESS2_PLUGINS__UTILS_HPP_
---
+++ mess2_ws/src/mess2_plugins/src/utils.cpp
@@ -0,0 +1,12 @@
+
+#include "mess2_plugins/utils.hpp"
+
+namespace mess2_plugins {
+
+    std::string get_vicon_topic(std::string agent_name) 
+    {
+        std::string topic = "/vicon/" + agent_name + "/" + agent_name;
+        return agent_name;
+    }
+
+}
